1db1f1b539677fad1be63c69fc83192f5e72aaac
Şerif <gungoronline@gmail.com>
2022-03-19 22:06:43 +0300

    Create AssetsBundleFromUrl.cs
    You can create and show scene with download from url

---
+++ Assets/AssetsBundleFromUrl.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AssetsBundleFromUrl : MonoBehaviour
+{
+    public string assetName;
+    private AssetBundle assetBundle;
+
+    void Start()
+{
+        assetName = "demo";
+    StartCoroutine(DownloadAsset());
+}
+
+IEnumerator DownloadAsset()
+{
+        //Download asset bundle
+        WWW bundleWWW = WWW.LoadFromCacheOrDownload("http://localhost/demo", 1);
+        yield return bundleWWW;
+        assetBundle = bundleWWW.assetBundle;
+
+        if (assetBundle.isStreamedSceneAssetBundle)
+        {
+            string[] scenePaths = assetBundle.GetAllScenePaths();
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePaths[0]);
+            Application.LoadLevel(sceneName);
+        }
+
+    }
+}
